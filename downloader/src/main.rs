c51f5733a132be4ffb5ae7fc0ac07088c5cd2d63
Alona Enraght-Moony <code@alona.page>
2023-05-06 21:44:17 +0100

    Reliable??

--- downloader/src/main.rs
+++ downloader/src/main.rs
@@ -1,8 +1,9 @@
 use std::path::PathBuf;
 
-use anyhow::bail;
+use anyhow::{bail, Context};
 use regex::Regex;
-use tracing::{debug, info, trace};
+use reqwest::Request;
+use tracing::{debug, info, trace, warn};
 
 const RFC_INDEX_URL: &str = "https://www.rfc-editor.org/rfc-index.txt";
 
@@ -17,7 +18,8 @@ async fn do_main() -> anyhow::Result<()> {
         .user_agent("github.com/aDotInTheVoid/except")
         .build()?;
 
-    let index = client.get(RFC_INDEX_URL).send().await?.text().await?;
+    let index = // client.get(RFC_INDEX_URL).send().await?.text().await?;
+        include_str!("../rfc-index.txt");
 
     let Some((_, index)) = index
         .rsplit_once(
@@ -44,9 +46,10 @@ async fn do_main() -> anyhow::Result<()> {
 
     info!("found {} RFCs that need downloading", rfcs.len());
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<(PathBuf, String)>(100);
 
     let mut tasks = Vec::new();
+    info!("starting downloads");
     for i in &rfcs {
         let client = client.clone();
         let url = i.url();
@@ -54,31 +57,71 @@ async fn do_main() -> anyhow::Result<()> {
         let tx = tx.clone();
 
         tasks.push(tokio::spawn(async move {
-            let txt = client.get(&url).send().await?.text().await?;
+            let txt = get_url(&client, &url).await?;
             trace!(?path, "downloaded");
-            tx.send((path, txt)).await?;
+            tx.send((path, txt))
+                .await
+                .context("Couldn't send downloaded rfc")?;
 
             Result::<_, anyhow::Error>::Ok(())
         }));
     }
-
     info!("started all downloads");
 
-    tokio::task::spawn_blocking(move || -> anyhow::Result<()> {
+    let jh = tokio::task::spawn_blocking(move || -> anyhow::Result<()> {
         info!("starting writer");
         while let Some((path, txt)) = rx.blocking_recv() {
             trace!(?path, "writing");
-            std::fs::write(path, txt)?;
+            std::fs::create_dir_all(path.parent().unwrap())?;
+            std::fs::write(&path, txt)?;
+            trace!(?path, "wrote");
         }
         info!("done writing");
+        rx.close();
         Ok(())
     });
 
+    info!("waiting for {} downloads to finish", tasks.len());
+    let mut n_done = 0;
     for t in tasks {
-        t.await??;
+        match t.await? {
+            Ok(_) => {}
+            Err(e) => {
+                warn!(%e, "error downloading");
+            }
+        };
+        n_done += 1;
+        if n_done % 200 == 0 {
+            debug!(%n_done, "Making progress");
+        }
     }
+    info!("all downloads finished");
+
+    drop(tx);
+
+    jh.await??;
 
     Ok(())
+}
+
+async fn get_url(client: &reqwest::Client, url: &str) -> anyhow::Result<String> {
+    // async fn get_inner(client: &reqwest::Client, url: &str) -> Result<String, reqwest::Error> {
+    //     let txt =
+    //     Ok(txt)
+    // }
+
+    Ok(
+        backoff::future::retry(backoff::ExponentialBackoff::default(), || async move {
+            Ok(client
+                .get(url)
+                .send()
+                .await?
+                .error_for_status()?
+                .text()
+                .await?)
+        })
+        .await?,
+    )
 }
 
 #[derive(Debug)]
